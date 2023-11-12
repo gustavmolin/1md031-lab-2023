ea22b32171d500e3676f81c692fca91f01870ba9
gustavmolin <gustav.molin.01@gmail.com>
2023-11-12 23:32:12 +0100

    six

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,20 +1,34 @@
 <template>
-    <div>
-      {{ burger.name }} {{ burger.kCal }}
-    </div>
-  </template>
+
+<div>
+        <h2 id="burgername">{{ burger.name }}</h2>
+
+        <img :src="burger.imgUrl" alt="Burger image" style="width: 200px;">
+        
+
+           <p> <ul>
+              <li> {{ burger.kCal +' '+ 'kCal'}}</li>
+              <li>Contains <span>{{ burger.containsLactose ? 'lactose' : 'no lactose' }}</span></li>
+              <li>Contains <span>{{ burger.containsGluten ? 'gluten' : 'no gluten' }}</span></li>
+            </ul> </p>
+        </div>
+    
+</template>
   
-  <script>
-  export default {
-    name: 'OneBurger',
-    props: {
-      burger: Object
-    }
+<script>
+export default {
+  props: {
+    burger:{
+      type: Object,
+    required: true
   }
-  </script>
+},
+mounted() {
+    console.log(this.burger);
+  }
+}
+</script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  
-  </style>
+<style scoped></style>
   
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,124 @@
 <template>
-  <div>
-    <div>
-      <h1>Burgers</h1>
-      <Burger v-for="burger in burgers"
-              v-bind:burger="burger" 
-              v-bind:key="burger.name"/>
-    </div>
-    <div id="map" v-on:click="addOrder">
-      click here
-    </div>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="resetsheet" type="text/css" href="reset.css">
+    <title>Hamburgarstugan</title>
+    <meta charset="utf-8" />
+  </head>
+
+  <div id="wholeheader">
+    <img src="https://klimatkommunerna.se/wp-content/uploads/2021/05/uppsala-kommun-scaled.jpg" id="headpicture">
+    <header>
+      <h1>Welcome to Hamburgarstugan!</h1>
+
+    </header>
+
+
   </div>
+  <main>
+    <section id="burgerwindow">
+      <Burger v-for="burger in menu" :key="burger.name" :burger="burger" />
+    </section>
+
+
+
+    <section id="orderinfo">
+
+      <h4>About us:</h4>We are a local hamburger restaurant located in Uppsala. Welcome!
+
+
+      <form>
+        <fieldset>
+          <p>
+            <label for="fullname">Your name</label><br>
+            <input type="text" id="fullname" name="fn" required="required" placeholder="First- and Last name">
+          </p>
+
+          <p>
+            <label for="email">E-mail</label><br>
+            <input type="text" id="email" name="em" required="required" placeholder="E-mailaddress">
+          </p>
+          <p>
+            <label for="streetname">Street</label><br>
+            <input type="text" id="streetname" name="ln" placeholder="Streetname">
+          </p>
+          <p>
+            <label for="housenumber">House</label><br>
+            <input type="number" id="housenumber" name="ln" placeholder="Housenumber">
+          </p>
+          <p>
+            <label for="payment">Choose payment method</label>
+
+            <select id="payment" name="pay">
+              <option>Card</option>
+              <option>Swish</option>
+              <option>Invoice</option>
+              <option>Cash</option>
+            </select>
+          </p>
+          <div>
+            <legend>Gender:</legend>
+            <div>
+              <input type="radio" id="Male" name="drone" value="Male" checked />
+              <label for="Male">Male</label>
+            </div>
+
+            <div>
+              <input type="radio" id="Female" name="drone" value="Female" />
+              <label for="Female">Female</label>
+            </div>
+
+            <div>
+              <input type="radio" id="Do not wish to provide" name="drone" value="Do not wish to provide" />
+              <label for="Do not wish to provide">Do not wish to provide</label>
+            </div>
+          </div>
+        </fieldset>
+      </form>
+    </section>
+    <br>
+    <button type="button">
+      <img
+        src="https://img.freepik.com/premium-vector/free-shipping-sticker-free-delivery-badge-with-truck-vector-isolated-background-eps-10_399089-2425.jpg?w=740"
+        style="width: 30px;">
+      Send Info
+    </button>
+
+  </main>
+
+  <footer>
+    <hr> Hamburgarstugan AB 2023 &copy;
+    <hr>
+  </footer>
 </template>
+
+
 
 <script>
 import Burger from '../components/OneBurger.vue'
 import io from 'socket.io-client'
+import menu from '../assets/menu.json'
+
 
 const socket = io();
 
+//Define Menuitem cunstructor function
+
+
+
+/*function MenuItem(name, imgUrl, kCal, containsGluten, containsLactose) {
+  this.name = name;
+  this.imgUrl = imgUrl;
+  this.kCal = kCal;
+  this.containsGluten = containsGluten;
+  this.containsLactose = containsLactose;
+}
+const burgerarray = [
+  new MenuItem("Mr Bacon", "https://img.freepik.com/free-photo/craft-beef-burger-french-fries-wooden-table-melting-chesses-isolated-black-background-ai-generative_157027-1745.jpg?w=1380&t=st=1699200430~exp=1699201030~hmac=a6f742dfe670165249fcd12c152a7fde43a55e42b222489fcf30961ceb89b39f", "250", false, false),
+  new MenuItem("Mr Cheese", "https://img.freepik.com/free-photo/grilled-beef-burger-with-cheese-tomato-generated-by-ai_24640-82315.jpg?t=st=1699197258~exp=1699200858~hmac=7fe225706fe8da5296e4673cc461c48e76d6579bc7f5ac43a8a71b00cf6b8f36&w=1380", "450", false, true),
+  new MenuItem("Mr Chicken", "https://img.freepik.com/free-photo/burger-with-mayonnaise-sauce-it_188544-15976.jpg?t=st=1699200117~exp=1699203717~hmac=2661f7759652976bc37fa0d93b78c1dbcedab8c29cdc5779a4cf78f7865ee2b6&w=1380", "850", true, false)
+];
+*/
 export default {
   name: 'HomeView',
   components: {
@@ -25,34 +126,130 @@ export default {
   },
   data: function () {
     return {
-      burgers: [ {name: "small burger", kCal: 250},
-                 {name: "standard burger", kCal: 450},
-                 {name: "large burger", kCal: 850}
-               ]
-    }
+      menu
+    };
   },
+
   methods: {
     getOrderNumber: function () {
-      return Math.floor(Math.random()*100000);
+      return Math.floor(Math.random() * 100000);
     },
     addOrder: function (event) {
-      var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                    y: event.currentTarget.getBoundingClientRect().top};
-      socket.emit("addOrder", { orderId: this.getOrderNumber(),
-                                details: { x: event.clientX - 10 - offset.x,
-                                           y: event.clientY - 10 - offset.y },
-                                orderItems: ["Beans", "Curry"]
-                              }
-                 );
+      var offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top
+      };
+      socket.emit("addOrder", {
+        orderId: this.getOrderNumber(),
+        details: {
+          x: event.clientX - 10 - offset.x,
+          y: event.clientY - 10 - offset.y
+        },
+        orderItems: ["Beans", "Curry"]
+      }
+      );
     }
   }
 }
 </script>
 
 <style>
-  #map {
-    width: 300px;
-    height: 300px;
-    background-color: red;
+h1 {
+  color: black;
+  margin-right: 30px;
+  /*font-size: 5em;*/
+  font-family: arial;
+
+
+}
+
+
+
+.main,
+header,
+footer,
+nav ul {
+  max-width: 80rem;
+  margin: 0 auto 0 auto;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 33% 33% 33%;
+  background-color: black;
+  color: white;
+}
+
+.box {
+  background-color: black;
+  color: #fff;
+  border-radius: 5px;
+  padding: 20px;
+  font-size: 110%;
+}
+
+
+#burgerwindow {
+  color: white;
+}
+
+header h1 {
+  position: relative;
+  text-align: center;
+  margin-top: -48%;
+
+
+}
+
+@media screen and (min-width: 1920px) {
+  header h1 {
+    margin-top: -90%;
   }
+}
+
+#wholeheader {
+  margin-left: 10px;
+  margin-right: 10px;
+  position: relative;
+  overflow: hidden;
+  height: 220px;
+  width: 100%;
+}
+
+#headpicture {
+  opacity: 0.65;
+  width: 100%;
+  height: auto;
+
+
+}
+
+section {
+  padding: 5px;
+  margin: 6px;
+}
+
+
+button:hover {
+  background-color: gray;
+  transition: 0.7s;
+  cursor: pointer;
+}
+
+#burgername {
+  margin-left: 20px;
+}
+
+#burgerwindow {
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  justify-items: center;
+  align-items: center;
+  background-color: #000000;
+  width: 100%;
+  margin: auto;
+
+}
 </style>
